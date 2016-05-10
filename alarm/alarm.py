44adfda03e02133c7bb387d1963e503c1c28747c
centip3de <goodkingkyle@gmail.com>
2016-05-10 10:56:12 -0700

    Added prototype for multiple alarm types, filepath parsing, and a basic settings file

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import alarm_settings
 from jaratoolbox import settings
 from jaratoolbox import loadbehavior
 from jaratoolbox import behavioranalysis
@@ -11,12 +12,26 @@ from email.mime.text import MIMEText
 class Alarm(object):
     # TODO: This should most likely be running constantly, or whenever new data is availible to check. 
 
-    def __init__(self, threshold):
+    def __init__(self, threshold = 0, belowThreshold = False, aboveThreshold = False, missingData = False):
         # A basic threshold, to detect if average performance falls below it. 
         self.behavData      = [] 
+
+        # Flags for certain alarms
         self.threshold      = threshold
+        self.belowThreshold = belowThreshold
+        self.aboveThreshold = aboveThreshold
+        self.missingData    = missingData
+
+        # Information from parsed filepath
+        self.subjectName        = None
+        self.experimentData     = None
+        self.paradigm           = None
+        self.experimenterName   = None
+        self.experimentDate     = None
 
     def loadData(self):
+        # Load all the data availible into a list for later processing.
+
         for info in os.walk(settings.BEHAVIOR_PATH):
             path = info[0]
 
@@ -24,16 +39,27 @@ class Alarm(object):
                 name, extension = element.split(".")
                 if(extension == "h5"):
                     full_path = os.path.join(path, element)
-                    self.behavData.append((name, loadbehavior.FlexCategBehaviorData(full_path, readmode='full')))
+                    self.behavData.append((full_path, loadbehavior.FlexCategBehaviorData(full_path, readmode='full')))
+
+    def parseFilePath(self, filepath):
+        li                      = filepath.split("/") 
+        last                    = li[-1].split("_")
+
+        self.subjectName        = li[-2]
+        self.experimenterName   = li[-3]
+        self.experimentDate     = last[-1]
+        self.paradigm           = last[-2]
+        self.subjectName        = last[-3]
 
     def alert(self, sender, destination, password):
-        # TODO: Multiple types of alerts (e.g. No data for a certain animal, average below and above a certain threshold, etc.)
         # TODO: Probably shouldn't be storing the user/password in plaintext
 
-        for name, data in self.behavData:
-            if(self.calculateAverage(data) < self.threshold):
+        for path, data in self.behavData:
+            self.parseFilePath(path)
+            if(self.calculateAverage(data) < self.threshold and self.belowThreshold):
 
-                print("Average below acceptable amount for " + name + ". Sending alert.")
+                print("Average below acceptable amount for " + self.subjectName + ". Sending alert to " + 
+                        self.experimenterName +  " using " + alarm_settings.contact_dict[self.experimenterName][0] + ".")
 
                 """
                 To test the email sending capability, replace "username@uoregon.edu" with a testing username, 
@@ -55,6 +81,14 @@ class Alarm(object):
                 conn.close()
                 """
 
+            if(self.calculateAverage(data) > self.threshold and self.aboveThreshold):
+                print("Average above acceptable amount for " + self.subjectName + ". Sending alert to " + 
+                        self.experimenterName +  " using " + alarm_settings.contact_dict[self.experimenterName][0] + ".")
+
+            if(self.missingData):
+                # TODO
+                pass
+
     def calculateAverage(self, data):
         nValidTrials        = data['nValid'][-1]
         nRewardTrials       = data['nRewarded'][-1]
@@ -62,7 +96,7 @@ class Alarm(object):
 
 def main():
     # Set the threshold so high it will always alert (for testing purposes)
-    alarm = Alarm(1)
+    alarm = Alarm(threshold = 1, belowThreshold = True)
     alarm.loadData()
     alarm.alert("username@uoregon.edu", "username@uoregon.edu", "password") 
 
---
+++ alarm/alarm_settings.py
@@ -0,0 +1,4 @@
+
+contact_dict = {}
+contact_dict['test']        = ("test_email@uoregon.edu", None)
+contact_dict['santiago']    = ("santiago_email@uoregon.edu", None)
