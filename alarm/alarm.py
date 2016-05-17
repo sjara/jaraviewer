e9fb7cdfdfd64c5a45931adbe51676b42e468cd9
centip3de <goodkingkyle@gmail.com>
2016-05-17 10:16:46 -0700

    Changed config to be per animal config, abstracted out emailing method, created list of animals to check for in settings file.

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -51,46 +51,46 @@ class Alarm(object):
         self.paradigm           = last[-2]
         self.subjectName        = last[-3]
 
+    def sendEmail(self, sender, destination):
+        smtpServer      = "smtp.uoregon.edu"
+        textSubtype     = "plain"
+
+        # Setup message variables 
+        msg             = MIMEText("This is a test alert.")
+        msg['Subject']  = "Alarm alert: Performance below threshold"
+        msg['From']     = sender 
+
+        # Setup the connection, and attempt to send the message.
+        conn            = SMTP(smtpServer)
+        conn.set_debuglevel(False)
+        conn.login(sender, password)
+        conn.sendmail(sender, destination, msg.as_string())
+        conn.close()
+
+
     def alert(self):
-        # TODO: Probably shouldn't be storing the user/password in plaintext
+        animals = alarm_settings.list_of_animals
 
         for path, data in self.behavData:
             self.parseFilePath(path)
+            if self.subjectName in animals:
+                animals.remove(self.subjectName)
+
             if(self.calculateAverage(data) < self.threshold and self.belowThreshold):
+                # TODO: This should be read in from the file. 
+                sender = "foo@bar.baz" 
 
-                print("Average below acceptable amount for " + self.subjectName + ". Sending alert to " + 
-                        self.experimenterName +  " using " + alarm_settings.contact_dict[self.experimenterName][0] + ".")
+                for destination in alarm_settings.contact_dict[self.subjectName]:
+                    print("Average below acceptable amount for " + self.subjectName + ". Sending alert to " + destination)                    
+                    if(self.calculateAverage(data) > self.threshold and self.aboveThreshold):
+                            print(destination)
+                            #self.sendEmail(sender, destination)
+                            print("Average above acceptable amount for " + self.subjectName + ". Sending alert to " + destination) 
 
-                """
-                To test the email sending capability, replace "username@uoregon.edu" with a testing username, 
-                and the corresponding password below, before uncommenting.
-
-                sender          = alarm_settings.contact_dict["test"][0]
-                destination     = alarm_settings.contact_dict[self.experimenterName][0]
-
-                smtpServer      = "smtp.uoregon.edu"
-                textSubtype     = "plain"
-
-                # Setup message variables 
-                msg             = MIMEText("This is a test alert.")
-                msg['Subject']  = "Alarm alert: Performance below threshold"
-                msg['From']     = sender 
-
-                # Setup the connection, and attempt to send the message.
-                conn            = SMTP(smtpServer)
-                conn.set_debuglevel(False)
-                conn.login(sender, password)
-                conn.sendmail(sender, destination, msg.as_string())
-                conn.close()
-                """
-
-            if(self.calculateAverage(data) > self.threshold and self.aboveThreshold):
-                print("Average above acceptable amount for " + self.subjectName + ". Sending alert to " + 
-                        self.experimenterName +  " using " + alarm_settings.contact_dict[self.experimenterName][0] + ".")
-
-            if(self.missingData):
-                # TODO
-                pass
+        if(self.missingData and len(animals) != 0):
+            for animal in animals:
+                for destination in alarm_settings.contact_dict[animal]:
+                    print("Missing data for " + animal + ". Sending alert to " + self.experimenterName)
 
     def calculateAverage(self, data):
         nValidTrials        = data['nValid'][-1]
@@ -99,7 +99,7 @@ class Alarm(object):
 
 def main():
     # Set the threshold so high it will always alert (for testing purposes)
-    alarm = Alarm(threshold = 1, belowThreshold = True)
+    alarm = Alarm(threshold = 1, belowThreshold = True, missingData = True)
     alarm.loadData()
     alarm.alert()
 
--- alarm/alarm_settings.py
+++ alarm/alarm_settings.py
@@ -1,4 +1,7 @@
 
 contact_dict = {}
-contact_dict['test']        = ("test_email@uoregon.edu", None)
-contact_dict['santiago']    = ("santiago_email@uoregon.edu", None)
+contact_dict['adap021']         = ["foo_bar_baz@uoregon.edu"]
+contact_dict['adap022']         = ["test_email@uoregon.edu", "santiago_email@uoregon.edu", "random_lab_worker@uoregon.edu"]
+contact_dict['otherAnimal']     = ["santigao_email@uoregon.edu"]
+
+list_of_animals                 = ["adap021", "adap022", "otherAnimal"]
