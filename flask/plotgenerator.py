9d2f0a9b672ad1e385e7c946f7f5e1b638989a99
Jvargasw <jvargasw@uoregon.edu>
2016-05-17 13:04:39 -0700

    Added stress test
    Added a secondary option for testing within the Test() function.  This
    version creates 20 psychometrics and 20 dynamics in order to test how
    long it would take to do so (requested by Santiago). NOTE: Currently
    the function simply saves over the same file repeatedly, as I only have
    one set of test data. Therefore, only 2 svg files are generated by this
    mode, but each one is regenerated 20 times to prove timing.

--- flask/plotgenerator.py
+++ flask/plotgenerator.py
@@ -59,12 +59,24 @@ def Test():
     #end Santiago's code
     #make each dictionary (for test, 1 per graph type) and append them to the list
     graphList = []
+    '''
+    #Test 1: Simple one of each graph test
+    
     graphDict1 = {'type' : "psychometric", 'filename' : "adap021_20160310_psychometric.svg", 'data' : behavData}
     graphList.append(graphDict1)
     graphDict2 = {'type' : "summary", 'filename' : "adap021_20160310_summary.svg", 'data' : behavData}
     graphList.append(graphDict2)
     graphDict3 = {'type' : "dynamics", 'filename' : "adap021_20160310_dynamics.svg", 'data' : behavData}
     graphList.append(graphDict3)
+    '''
+    #'''
+    #Test2: stress test. 20 summary, 20 dynamics
+    graphDictPsy = {'type' : "psychometric", 'filename' : "adap021_20160310_psychometric.svg", 'data' : behavData}
+    graphDictDyn = {'type' : "dynamics", 'filename' : "adap021_20160310_dynamics.svg", 'data' : behavData}
+    for i in range(20):
+        graphList.append(graphDictPsy)
+        graphList.append(graphDictDyn)
+    #'''
     #time.time returns the current system time in seconds. by taking the time before and after running the Generate function and taking the difference,
     #we can see the exact time in seconds that the module took to run.
     t0 = time.time()
