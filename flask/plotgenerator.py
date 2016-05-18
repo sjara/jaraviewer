16ae7235dce13a59b39ac23286825177b7c0cc9b
Jvargasw <jvargasw@uoregon.edu>
2016-05-18 14:30:19 -0700

    Moved tests to separate file + added filepaths to plotgenerator.py
    Made the change to the plotgenerator.py file that allows it to use the
    file path from the settings file. NOTE: Due to my previous issues with
    installing jaratoolbox, I can’t test this from the folder in Git (since
    I have to be in my root in order to import the toolbox). In theory, it
    should work fine. However, if issues with the code arises let me know
    so I can attempt to fix them.

--- flask/plotgenerator.py
+++ flask/plotgenerator.py
@@ -2,7 +2,7 @@
 #By: James Vargas-Witherell
 import matplotlib.pyplot as plt
 from jaratoolbox import behavioranalysis
-
+import settings
 '''
 INPUT CONVENTIONS:
 plotList should be a list of dictionaries, each dictionary with the following parameters:
@@ -13,7 +13,7 @@ plotList should be a list of dictionaries, each dictionary with the following pa
 A parameter may be added that contains the filepath to the graph storage folder. Need to discuss
 '''
 #Path variable. Currently is an empty string, so files will be stored in the current directory
-path = ""
+path = settings.IMAGE_PATH
 def Generate(plotList):
 #plotList is the list of plot dictionaries as specified above
     for plot in plotList:
@@ -33,54 +33,9 @@ def Generate(plotList):
         else:
             #ERROR case: plot type invalid
             return False
-        #save the plot under the given filename (need to edit with filepath)
-        #NOTE: Current savefig will save to whatever directory the code is run in. Be warned
-        plt.savefig(plot['filename'])
-        ### When settings file is done, and path is avalible, uncomment below code and delete the above one ###
-        #plt.savefig(os.path.join(path, plot['filename']))
+        #save the plot under the given filepath
+        plt.savefig(os.path.join(path, plot['filename']))
     return True 
 
-#the following are only needed for the test function. 
-from jaratoolbox import loadbehavior
-from jaratoolbox import settings
-import time
-def Test():
-    #Test function: Used to test code without needing the other modules
-    # NOTE: this text function will be a mini version of what the backend will be doing each time
-    #code from "test032_example_read_bdata.py" by Santiago Jaramillo
-    EXPERIMENTER = settings.DEFAULT_EXPERIMENTER
-    paradigm = '2afc'
-    subject = 'adap021'
-    session = '20160310a' # This is the date formatted as YYYYMMDD and one more character (usually 'a')
 
-    # -- Find the data filename and load the data into a data object (similar to a Python dict) --
-    behavFile = loadbehavior.path_to_behavior_data(subject,EXPERIMENTER,paradigm,session)
-    behavData = loadbehavior.FlexCategBehaviorData(behavFile,readmode='full')
-    #end Santiago's code
-    #make each dictionary (for test, 1 per graph type) and append them to the list
-    graphList = []
-    '''
-    #Test 1: Simple one of each graph test
-    
-    graphDict1 = {'type' : "psychometric", 'filename' : "adap021_20160310_psychometric.svg", 'data' : behavData}
-    graphList.append(graphDict1)
-    graphDict2 = {'type' : "summary", 'filename' : "adap021_20160310_summary.svg", 'data' : behavData}
-    graphList.append(graphDict2)
-    graphDict3 = {'type' : "dynamics", 'filename' : "adap021_20160310_dynamics.svg", 'data' : behavData}
-    graphList.append(graphDict3)
-    '''
-    #'''
-    #Test2: stress test. 20 summary, 20 dynamics
-    graphDictPsy = {'type' : "psychometric", 'filename' : "adap021_20160310_psychometric.svg", 'data' : behavData}
-    graphDictDyn = {'type' : "dynamics", 'filename' : "adap021_20160310_dynamics.svg", 'data' : behavData}
-    for i in range(20):
-        graphList.append(graphDictPsy)
-        graphList.append(graphDictDyn)
-    #'''
-    #time.time returns the current system time in seconds. by taking the time before and after running the Generate function and taking the difference,
-    #we can see the exact time in seconds that the module took to run.
-    t0 = time.time()
-    Generate(graphList)
-    t1 = time.time()
-    print(t1-t0)
 
