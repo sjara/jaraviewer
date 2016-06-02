203741fcd511699c87aa3c6d79ad4b5ed38e64c1
Jvargasw <jvargasw@uoregon.edu>
2016-06-02 16:44:23 -0700

    Modified plotgenerator.py in the main directory to do only one plot at a time at the request of HongLux and Sjara (see issue#22)
    Edited Santiago’s version of the plot generator to work with a single
    plot at a time rather than a list of plots at the request of the
    comment in issue #22

--- plotgenerator.py
+++ plotgenerator.py
@@ -10,7 +10,7 @@ import os
 
 FONTSIZE = 14
 
-def generate(plotList):
+def generate(plotInfo):
     '''
     FIXME: finish this docstring
 
@@ -21,20 +21,19 @@ def generate(plotList):
     'data': raw behavior data created by jaratoolbox.loadbehavior.FlexCategBehaviorData()
 
     Files are saved to settings.IMAGE_PATH
-    '''
-    for plotInfo in plotList:
-        plotData = plotInfo['data']
-        if plotInfo['type'] == 'psychometric':
-            behavioranalysis.plot_frequency_psycurve(plotData,fontsize=FONTSIZE)
-        elif plotInfo['type'] == 'summary':
-            freqsToUse = [plotData['lowFreq'][-1],plotData['highFreq'][-1]]
-            behavioranalysis.plot_summary(plotData,fontsize=FONTSIZE,soundfreq=freqsToUse)
-        elif plotInfo['type'] == 'dynamics':
-            freqsToUse = [plotData['lowFreq'][-1],plotData['highFreq'][-1]]
-            behavioranalysis.plot_dynamics(plotData,winsize=40,fontsize=FONTSIZE,soundfreq=freqsToUse)
-        else:
-            raise ValueError('Plot type {0} is not valid'.format(plotInfo['Type']))
-        outputFile = os.path.join(settings.IMAGE_PATH, plotInfo['filename'])
-        print 'Saving plot to {0}'.format(outputFile)
-        plt.savefig(outputFile)
+   : '''
+    plotData = plotInfo['data']
+    if plotInfo['type'] == 'psychometric':
+        behavioranalysis.plot_frequency_psycurve(plotData,fontsize=FONTSIZE)
+    elif plotInfo['type'] == 'summary':
+        freqsToUse = [plotData['lowFreq'][-1],plotData['highFreq'][-1]]
+        behavioranalysis.plot_summary(plotData,fontsize=FONTSIZE,soundfreq=freqsToUse)
+    elif plotInfo['type'] == 'dynamics':
+        freqsToUse = [plotData['lowFreq'][-1],plotData['highFreq'][-1]]
+        behavioranalysis.plot_dynamics(plotData,winsize=40,fontsize=FONTSIZE,soundfreq=freqsToUse)
+    else:
+        raise ValueError('Plot type {0} is not valid'.format(plotInfo['Type']))
+    outputFile = os.path.join(settings.IMAGE_PATH, plotInfo['filename'])
+    print 'Saving plot to {0}'.format(outputFile)
+    plt.savefig(outputFile)
 
